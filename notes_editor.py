166727c482e3a47d0f85d4c14ee55953eeaab8b3
EdwardJoko <edwardjptatuhas@outlook.com>
2020-10-29 17:30:41 +0700

    improve the naming and fix one bug

--- notes_editor.py
+++ notes_editor.py
@@ -1,4 +1,4 @@
-def file_string(text):
+def edit_file(text):
     with open(text, 'r') as file:
         lines = file.read().split('\n')
     file.close()
@@ -14,41 +14,42 @@ def file_string(text):
             new_file.write('\n')
         else:
             j = 0
-            x = len(line)
-            while x > 0:
-                if x > 64:  # the sign was >= and it's a bug ever since array
-                            # of 5, there is no element in array[5] because
-                            # maksimum is array[4]
+            line_length = len(line)   # x == line_length
+            while line_length > 0:
+                if line_length > 64:  # the sign was >= and it's a bug ever since in array of 5,
+                                      # there last element is not at array[5] but array[4]
                     i = j
                     j += 64
                     if line[j] == ' ':
                         new_file.write(line[i: j])
                         new_file.write('\n')
                     else:
-                        while line[j] != ' ':
+                        while line[j] != ' ' and j != -1:
                             j -= 1
                         new_file.write(line[i: j])
                         new_file.write('\n')
-                    x -= (j - i)
+                        if j == -1:
+                            break
+                    line_length -= (j - i)
                 else:
-                    new_file.write(line[j: j+x+1])
+                    new_file.write(line[j: j+line_length+1])
                     new_file.write('\n')
-                    x = 0
+                    line_length = 0
     new_file.close()
 
 if __name__ == '__main__':
     codes = []
-    strings = input("Give me the files...\n")
-    strings += " "      # so that the else in line 47 will be execute at the end of the loop
-    cod = ''
-    for i in strings:
-        if i != ' ':
-            cod += i
+    files = input("Give me the files...\n")     # a string
+    files += " "      # so that the else in line 48 will be execute at the end of the loop
+    file = ''
+    for char in files:
+        if char != ' ':
+            file += char
         else:
-            codes.append(cod)
-            cod = ''
+            codes.append(file)
+            file = ''
     while '' in codes:
         codes.remove('')
     for code in codes:
-        file_string(code)
+        edit_file(code)
     print(f"All of these documents is successfully edited!\n\t{codes}")
