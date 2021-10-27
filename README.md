6ceb72bd850e48a212084449901936b4503b658d
Joko <edo@bfi.io>
2021-10-27 21:59:02 +0700

    Improve code readability and add readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# test
+### Notes Editor
+
+I made this script on 2019 to automatically edit the notes that I copied from [freeCodeCamp](https://www.freecodecamp.org/learn/).
+
+It changes the notes from like this, (the result of copying directly from freeCodeCamp)
+
+![before](/images/before.png "before")
+
+to like this, (maximum just 64 characters per line)
+
+![after](/images/after.png "after")
+
+
+This script can accept and edit more than 1 file.
--- notes_editor.py
+++ notes_editor.py
@@ -1,55 +1,68 @@
-def edit_file(text):
-    with open(text, 'r') as file:
-        lines = file.read().split('\n')
+def edit_file(file_name):
+    # read the text of the file and split it according to newline,
+    # and store the content to 'lines' variable
+    file = open(file_name, "r")
+    lines = file.read().split("\n")
     file.close()
 
-    # in case we want to delete empty line uncomment below
-    # while '' in lines:
-    #     lines.remove('')
+    # replace the file that we edit by creating a new file with the same name
+    new_file = open(file_name, "w")
 
-    new_file = open(text, 'w')
+    max_char_per_line = 64
+    # loop through all the text from the file
     for line in lines:
-        if len(line) < 64:
+        line = line.strip()
+        # simply just write it to the file, if the length of the line is less/equal than 64
+        if len(line) <= max_char_per_line:
             new_file.write(line)
-            new_file.write('\n')
+            new_file.write("\n")
         else:
-            j = 0
-            line_length = len(line)   # x == line_length
-            while line_length > 0:
-                if line_length > 64:  # the sign was >= and it's a bug ever since in array of 5,
-                                      # there last element is not at array[5] but array[4]
-                    i = j
-                    j += 64
-                    if line[j] == ' ':
-                        new_file.write(line[i: j])
-                        new_file.write('\n')
-                    else:
-                        while line[j] != ' ' and j != -1:
-                            j -= 1
-                        new_file.write(line[i: j])
-                        new_file.write('\n')
-                        if j == -1:
-                            break
-                    line_length -= (j - i)
+            while len(line) > max_char_per_line:
+                i = max_char_per_line
+                # if the 65th char is " ", then write the text from 0 to 64
+                if line[i] == " ":
+                    new_file.write(line[0: i])
+                    new_file.write("\n")
+                    line = line[i: len(line)]
+                # if the 65th char is not " ",
                 else:
-                    new_file.write(line[j: j+line_length+1])
-                    new_file.write('\n')
-                    line_length = 0
+                    # then we look for (<65)th char that is " "
+                    while line[i] != " " and i != -1:
+                        i -= 1
+                    # if we couldn't find the " ", because the word is longer than 64 chars
+                    # then we cut the word, and then put "-" at the end
+                    if i == -1:
+                        new_file.write(line[0: max_char_per_line])
+                        new_file.write("-")
+                        # we delete the string that already written
+                        line = line[max_char_per_line: len(line)]
+                    # if we can find the " " at the (<65)th char, then we write it
+                    else:
+                        new_file.write(line[0: i])
+                        # we delete the string that already written
+                        line = line[i: len(line)]
+                    new_file.write("\n")
+            else:
+                new_file.write(line)
+                new_file.write("\n")
+    
+    # close and save the file
     new_file.close()
 
-if __name__ == '__main__':
-    codes = []
-    files = input("Give me the files...\n")     # a string
-    files += " "      # so that the else in line 48 will be execute at the end of the loop
-    file = ''
-    for char in files:
-        if char != ' ':
-            file += char
-        else:
-            codes.append(file)
-            file = ''
-    while '' in codes:
-        codes.remove('')
-    for code in codes:
-        edit_file(code)
-    print(f"All of these documents is successfully edited!\n\t{codes}")
+# it starts here
+if __name__ == "__main__":
+    # asking input the names of the files
+    input_desc = "Give me all the files...\n" \
+            "Separate the name of the files with a space.\n" \
+            "For example: \'file_1 file_2\'\n\n"
+    files_string = input(input_desc)
+
+    # split the string of the input according to space,
+    # so we have an array that consists of files' name
+    files_array = files_string.split()
+
+    # edit all the files with loop
+    for file in files_array:
+        edit_file(file)
+    
+    print(f"All of these documents is successfully edited!\n\t{files_array}")
